a3cd0177ddca49acd886b02a499447c28789017c
Hungnha <fruirt35@gmail.com>
2026-05-12 14:08:58 +0700

    header

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// NOTE: Bootstrap đã được thay thế bởi Tailwind CSS (qua CDN trong index.html)
-// Chỉ giữ lại CSS gốc nếu cần
-// import './assets/main.css'
+// NOTE: Bootstrap đã được thay thế bởi Tailwind CSS
+import './assets/main.css'
 
 const app = createApp(App)
 
